302b166c625e3916fccae913069976bf336062d4
jdc141 <joecharland14@gmail.com>
2023-07-25 10:30:32 -0400

    Google Docs Clone, finished product

---
+++ client/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import App from "./App";
+import  ReactDOM from "react-dom";
+import "./styles.css"
+
+
+ReactDOM.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+);
+
+
